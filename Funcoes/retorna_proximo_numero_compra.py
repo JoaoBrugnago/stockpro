0967dcbfa76a19e98807bb9f138bc17bbae3a5ee
Joao Pedro Soares Brugnago <joao.psb20@gmail.com>
2023-12-22 20:50:59 -0300

    Alterado repositorio retorna_proximo_numero_compra

--- Funcoes/retorna_proximo_numero_compra.py
+++ Funcoes/retorna_proximo_numero_compra.py
@@ -8,14 +8,21 @@ class ProximaCompra:
     self.conn_str = bancoDeDados.conn_str
 
   def proximoValor(self):
-    conn = pyodbc.connect(self.conn_str)
-    cursor = conn.cursor()
+    conn = None
+    try:
+      conn = pyodbc.connect(self.conn_str)
+      cursor = conn.cursor()
 
-    select_ultimo_cmpcode = 'SELECT TOP 1 cmpcode FROM compras ORDER BY cmpcode DESC'
-    cursor.execute(select_ultimo_cmpcode)
-    resultado = cursor.fetchone()
+      select_ultimo_cmpcode = 'SELECT TOP 1 cmpcode FROM compras ORDER BY cmpcode DESC'
+      cursor.execute(select_ultimo_cmpcode)
+      resultado = cursor.fetchone()
 
-    CmpCode = int(resultado[0]) + 1 if resultado else 1
-    conn.close()
+      CmpCode = int(resultado[0]) + 1 if resultado else 1
+      return CmpCode
     
-    return CmpCode
+    except Exception as e:
+      print(f'Erro: {e}')
+
+    finally:
+      if conn:
+        conn.close()
