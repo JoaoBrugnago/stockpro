6ab5762ec69a0e0a34a8057bc54f9b056fddd119
GuilhermeGBarreto <guilherme.gabriel.barreto@gmail.com>
2023-12-23 13:17:11 -0300

    update main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# Apenas testando
+from Funcoes.retorna_proximo_numero_compra import ProximaCompra
+
+proxima_compra = ProximaCompra()
+
+codigo_produto = proxima_compra.proximoValor()
+print(codigo_produto)
